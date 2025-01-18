9dba5a79a8bc991089c8805dde587e5b426a9d9c
anderregidor <krinkuto11@protonmail.com>
2025-01-18 18:34:45 +0100

    Deleted more imports

--- Herramientas/General.py
+++ Herramientas/General.py
@@ -3,19 +3,11 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 import os
 import glob
-import math
 
 def traducir_predicciones(predicciones):
-    """
-    Traduce una lista de predicciones en formato string ('0'-'35') a caracteres ('0'-'9' y 'a'-'z' sin 'ñ').
 
-    :param predicciones: Lista de predicciones en formato string o errores ("ERROR: NaN en predicción")
-    :return: Lista con las predicciones traducidas a caracteres
-    """
-    # Crear un diccionario de mapeo: 0-9 → '0'-'9', 10-35 → 'a'-'z' (sin ñ)
     num_to_char = {str(i): chr(i + 48) if i < 10 else chr(i + 87) for i in range(36)}
 
-    # Lista para almacenar los caracteres traducidos
     traducciones = []
 
     for pred in predicciones:
